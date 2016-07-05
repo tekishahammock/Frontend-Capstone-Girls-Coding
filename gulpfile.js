f6776180635e1f6f94bd2897c6e7eca575e82f71
Tekisha Hammock <tekisha.hammock@gmail.com>
2016-07-05 14:43:16 -0500

    re-initialized project with cleaned up project set-up files

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+// RUN USING "npm run serve" AFTER INSURING THE FOLLOWING CODE IS IN PACKAGE.JSON:
+    // "build": "gulp build",
+    // "serve": "gulp watch",
+    // "start": "npm run build && http-server dist",
+    // "test": "echo \"Error: no test specified\" && exit 1"
+// PUT THIS UNDER "scripts"
